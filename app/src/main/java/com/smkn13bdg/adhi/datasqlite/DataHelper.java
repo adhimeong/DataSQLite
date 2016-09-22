dc1549d193b2bdb468945cac47e68064ff9ad9de
adhimeong <adhimeong@gmail.com>
2016-09-22 10:12:35 +0700

    penambahan activity

--- app/src/main/java/com/smkn13bdg/adhi/datasqlite/DataHelper.java
+++ app/src/main/java/com/smkn13bdg/adhi/datasqlite/DataHelper.java
@@ -3,6 +3,7 @@ package com.smkn13bdg.adhi.datasqlite;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by adhi on 22/09/16.
@@ -10,14 +11,20 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DataHelper extends SQLiteOpenHelper {
 
-
+    private static final String DATABASE_NAME = "biodatadiri.db";
+    private  static final int DATABASE_VERSION = 1;
 
     public DataHelper(Context context) {
-        super(context, );
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE biodata(no integer primary key, nama text null, tgl text null, jk text null, alamat text null)";
+        Log.d("Data", "onCreate : " +sql);
+        db.execSQL(sql);
+        sql = "INSERT INTO biodata(no, nama, tgl, jk, alamat) VALUES ('101', 'Adhi Ismail Hasan','1999-02-03','laki-laki','bandung')";
+        db.execSQL(sql);
 
     }
 
---
+++ app/src/main/java/com/smkn13bdg/adhi/datasqlite/LihatBiodata.java
@@ -0,0 +1,13 @@
+package com.smkn13bdg.adhi.datasqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LihatBiodata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_biodata);
+    }
+}
